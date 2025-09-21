c8cddb2b7c61a597678cdb59b3427e63db1e7b52
Andy-Ops254 <ghostandy254@outlook.com>
2025-09-21 15:16:57 +0300

    complete chattebox lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,61 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods =['GET', 'POST'])
 def messages():
-    return ''
+    if request.method=='GET':
+        messages=[]
+        for message in Message.query.order_by(Message.created_at.asc()).all():
+            message_dict= message.to_dict()
+            messages.append(message_dict)
+        response = make_response(
+            messages,
+            200
+        )
+        return response
 
-@app.route('/messages/<int:id>')
+    elif request.method=='POST':
+        new_message = Message(
+        body = request.json.get("body"),
+        username =request.json.get("username")
+    )
+    db.session.add(new_message)
+    db.session.commit()
+
+    messages_dict = new_message.to_dict()
+    response = make_response(
+        messages_dict,
+        201
+    )
+    return response
+
+@app.route('/messages/<int:id>', methods=['DELETE', 'PATCH'])
 def messages_by_id(id):
-    return ''
+    query = Message.query.filter_by(id=id).first()
+    if request.method=='DELETE':
+        db.session.delete(query)
+        db.session.commit()
+        response_body = {
+            "delete_successful": True,
+            "Message": "Message has been deleted successfully",
+        }
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+
+    elif request.method=='PATCH':
+        for attr in request.json:
+            setattr(query, attr, request.json[attr])
+        db.session.add(query)
+        db.session.commit()
+        query_dict = query.to_dict()
+        response = make_response(
+            query_dict,
+            200
+        )
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
